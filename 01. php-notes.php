1609490e5da3e5aed69a28471a6d8d74b5e840d5
gitcommer <44485405+gitcommer@users.noreply.github.com>
2019-06-22 00:38:23 -0700

    Update 01. php-notes.php

--- 01. php-notes.php
+++ 01. php-notes.php
@@ -1,7 +1,11 @@
-
+- trap negative input and if order is greater than quantity
 
 
 
 
 $order_id = uniqid();  //uniqid is a tracking number
 id="'.$row["id"];  //you can add also db row on id in css
+
+trap negative input and if order is greater than quantity
+*********************************************************
+<input class="form-control" type="number" name="quantity" min="1" max="<?php echo $row["product_qty"]; ?>" value="1">
